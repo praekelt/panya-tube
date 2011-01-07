dfa05552546dd2d6cc2f3c841d3c42637ee5e99d
shaunsephton <shaun@praekeltconsulting.com>
2011-01-07 13:27:33 +0200

    added clip models

--- tube/models.py
+++ tube/models.py
@@ -2,52 +2,44 @@ from django.db import models
 
 from panya.models import ModelBase
 
-class AbstractClip(models.Model):
-    class Meta:
-        abstract = True
-
+class ClipBase(ModelBase):
+    """
+    Model defining clip metadata. Clip types should inherit from this model.
+    I.e. Dexter - Dex Lies and Videotape.
+    """
+    duration = models.IntegerField(
+        help_text="Duration of this clip in seconds. This is automatically determined from the media resource.",
+        blank=True,
+        null=True,
+    )
     age_restriction = models.ForeignKey(
         'tube.AgeRestriction',
         blank=True,
         null=True,
     )
-    show = models.ForeignKey(
-        'tube.Show',
-        help_text="Select a show for which this clip is an episode.",
-        blank=True,
-        null=True,
-    )
     channel = models.ForeignKey(
         'tube.Channel',
-        help_text="Select a channel on which this clip will be displayed. This field is only honoured when a show is not specified. If a show is specified its channel field takes priority.",
+        help_text="Select a channel for which this clip should be available. This field is only honoured when a series is not specified. If a series is specified its channel takes priority.",
         blank=True,
         null=True,
     )
-    season = models.IntegerField(
-        help_text="Season number of this clip.",
+    series = models.ForeignKey(
+        'tube.Series',
+        help_text="Select a series for which this clip is an episode.",
         blank=True,
         null=True,
     )
-    episode = models.IntegerField(
-        help_text="Episode number of this clip.",
-        blank=True,
-        null=True,
-    )
-    duration = models.IntegerField(
-        help_text="Duration of this clip in seconds. This is automatically determined from the uploaded clip and should only be manually specified if you really know what you are doing.",
-        blank=True,
-        null=True,
-    )
-
+    
     def get_channel(self):
         """
         Get channel for this clip.
-        Return the show's channel is show is populated, otherwise channel.
-        If no show or channel is populated return None.
+        Return the series' channel if series is populated, otherwise channel.
+        If no series or channel is populated return None.
         """
         # Return show's channel if this clip has a show associated with it.
-        if self.show and self.show.channel:
-            return self.show.channel
+        if self.series:
+            if self.series.channel:
+                return self.series.channel
         
         # Otherwise, return channel if this clip has a channel associated with it.
         if self.channel:
@@ -59,12 +51,51 @@ class AbstractClip(models.Model):
         channel = self.get_channel()
         return channel.content_id if channel else None
             
-    def get_show_title(self):
-        return self.show.title if self.show else ''
+    def get_series_title(self):
+        return self.series.title if self.series else ''
         
-    def get_show_id(self):
-        return self.show.id if self.show else ''
+    def get_series_id(self):
+        return self.series.id if self.series else ''
+    
+class Clip(VideoBase):
+    """
+    Clip model with local media resource.
+    """
+    media_resource = models.FileField(
+        help_text="Upload the media file for this clip.",
+        upload_to='tube_clip_resources',
+        blank=True,
+        null=True,
+    )
+    
+class Season(models.Model):
+    """
+    Model storing metadata associated with season.
+    I.e. Simpsons Season 2.
+    """
+    clips = models.ManyToManyField(
+        'tube.ClipBase',
+        through='tube.Episode',
+        blank=True,
+        null=True,
+    )
 
+class Episode(models.Model):
+    """
+    Model storing metadata associated with episode.
+    I.e. Dexter - Dex Lies and Videotape, Season 2 Episode 6.
+    """
+    clip = models.ForeignKey(
+        'tube.ClipBase',
+    )
+    season = models.ForeignKey(
+        'tube.Season',
+    )
+    episode_number = models.IntegerField(
+        help_text="Episode number of this clip.",
+        blank=True,
+        null=True,
+    )
 
 class AgeRestriction(models.Model):
     """
@@ -87,7 +118,7 @@ class AgeRestriction(models.Model):
 
 class Channel(ModelBase):
     """
-    Base channel model from which all Channel types should inherit.
+    Basic channel model.
     """
     age_restriction = models.ForeignKey(
         'tube.AgeRestriction',
