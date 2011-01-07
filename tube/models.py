8122b42df4310f417d3cac5a40df44b60c114958
shaunsephton <shaun@praekeltconsulting.com>
2011-01-07 10:32:50 +0200

    added abstractclip

--- tube/models.py
+++ tube/models.py
@@ -2,6 +2,70 @@ from django.db import models
 
 from panya.models import ModelBase
 
+class AbstractClip(models.Model):
+    class Meta:
+        abstract = True
+
+    age_restriction = models.ForeignKey(
+        'tube.AgeRestriction',
+        blank=True,
+        null=True,
+    )
+    show = models.ForeignKey(
+        'tube.Show',
+        help_text="Select a show for which this clip is an episode.",
+        blank=True,
+        null=True,
+    )
+    channel = models.ForeignKey(
+        'tube.Channel',
+        help_text="Select a channel on which this clip will be displayed. This field is only honoured when a show is not specified. If a show is specified its channel field takes priority.",
+        blank=True,
+        null=True,
+    )
+    season = models.IntegerField(
+        help_text="Season number of this clip.",
+        blank=True,
+        null=True,
+    )
+    episode = models.IntegerField(
+        help_text="Episode number of this clip.",
+        blank=True,
+        null=True,
+    )
+    duration = models.IntegerField(
+        help_text="Duration of this clip in seconds. This is automatically determined from the uploaded clip and should only be manually specified if you really know what you are doing.",
+        blank=True,
+        null=True,
+    )
+
+    def get_channel(self):
+        """
+        Get channel for this clip.
+        Return the show's channel is show is populated, otherwise channel.
+        If no show or channel is populated return None.
+        """
+        # Return show's channel if this clip has a show associated with it.
+        if self.show and self.show.channel:
+            return self.show.channel
+        
+        # Otherwise, return channel if this clip has a channel associated with it.
+        if self.channel:
+            return self.channel
+       
+        return None
+    
+    def get_channel_id(self):
+        channel = self.get_channel()
+        return channel.content_id if channel else None
+            
+    def get_show_title(self):
+        return self.show.title if self.show else ''
+        
+    def get_show_id(self):
+        return self.show.id if self.show else ''
+
+
 class AgeRestriction(models.Model):
     """
     Simple age restriction model.
