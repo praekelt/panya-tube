da3a0e399c13a2c1d31463acd4f4a95758473063
shaunsephton <shaun@praekeltconsulting.com>
2011-01-10 11:58:23 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='panya-tube',
-    version='0.0.1',
+    version='0.0.2',
     description='Panya tube(channel, show, clip) app.',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Foundation',
